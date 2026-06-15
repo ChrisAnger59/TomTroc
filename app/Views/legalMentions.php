e33368cb64bc4de39f7ff9800182a27556fde510
Christophe <christophe.anger.pro@gmail.com>
2026-06-15 12:32:45 +0200

    feat(footer_link): add legalMentions/privacyPolicy pages

---
+++ app/Views/legalMentions.php
@@ -0,0 +1,31 @@
+<section>
+    <article id="legal-mentions" class="flex-center flex-col gap-1">
+        <h1>Mentions légales</h1>
+
+        <h2>Propriété intellectuelle</h2>
+        <p>
+            L’ensemble des contenus présents sur ce site (textes, images, graphismes, logo, etc.) est protégé par le droit de la propriété intellectuelle. Toute reproduction, distribution ou utilisation sans autorisation préalable est interdite.
+        </p><br>
+
+        <h2>Responsabilité</h2>
+        <p>
+            L’éditeur du site s’efforce de fournir des informations aussi précises que possible. Toutefois, il ne pourra être tenu responsable des omissions, inexactitudes ou carences dans la mise à jour.
+        </p><br>
+
+        <h2>Données personnelles</h2>
+        <p>
+            Les informations personnelles collectées sont traitées conformément à la réglementation en vigueur. Pour plus d’informations, veuillez consulter notre politique de confidentialité.
+        </p><br>
+
+        <h2>Cookies</h2>
+        <p>
+            Le site peut utiliser des cookies afin d’améliorer l’expérience utilisateur. Vous pouvez configurer votre navigateur pour refuser ces cookies.
+        </p><br>
+
+        <h2>Droit applicable</h2>
+        <p>
+            Le présent site est soumis au droit applicable dans le pays d’exploitation. En cas de litige, les tribunaux compétents seront ceux du ressort du siège de l’éditeur.
+        </p><br>
+    </article>
+
+</section>
