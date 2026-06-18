26132b9da44b0197693f56cefe7814e01cd3c08a
Chris <christophe.anger.pro@gmail.com>
2026-06-18 14:59:55 +0200

    Feature/all books page (#7)
    * feat(allBooks+detailsBook pages): add allBooks and detailsBook pages
    
    * feat(AllBooks and detailsBook pages): add allBooks and detailsBook pages

--- app/Views/detailsBook.php
+++ app/Views/detailsBook.php
@@ -1,3 +1,35 @@
-<section>
-    <h1>Details du livre: <?= $book->getTitle() ?></h1>
+<p id="nav-pre-section">
+    <a href="index.php?action=allBooks">Nos livres</a> 
+    > 
+    <?= $book->getTitle() ?>
+</p>
+
+<section id="section-details" class="flex gap-2">
+
+    <div id="book-cover">
+        <img src="<?= $book->getImage() ?>">
+    </div>
+
+    <article id="book-article" class="flex-col">
+        <div id="title-author" class="flex-col gap-1">
+            <h1><?= $book->getTitle() ?></h1>
+            <p>par <?= $book->getAuthor() ?></p>
+        </div>
+
+        <span></span>
+
+        <div class="flex-col gap-1" id="description">
+            <h2>DESCRIPTION</h2>
+            <p><?= nl2br($book->getDescription()) ?></p>
+        </div>
+
+        <div class="flex-col gap-1">
+            <h2>PROPRIETAIRE</h2>
+            <div>
+                <img src=""><p><?= $book->getOwner() ?></p>
+            </div>
+        </div>
+
+        <button class="btn btn-primary">Envoyer un message</button>
+    </article>
 </section>
