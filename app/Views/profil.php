0a7c7c80e9f0a9dacbe537d5713ed2451d0a3e04
Christophe <christophe.anger.pro@gmail.com>
2026-06-30 17:03:27 +0200

    feat(ownedBookManagement): on 'Mon Compte' page

--- app/Views/profil.php
+++ app/Views/profil.php
@@ -1,15 +1,18 @@
-<section id="profile-info-section">
+<section id="profile-info-section" class="flex-col gap-2">
     <h2>Mon compte</h2>
     <article id="profile-info" class="flex">
 
         <div id="profile-resume" class="flex-col">
             <div class="flex-col">
                 <img id="profile-picture" src="<?= $user->getProfilePicturePath() ?>">
-                <a href="">Modifier</a>
+                <a href="">modifier</a>
+            </div>
+            <div id="border">
+
             </div>
             <div class="flex-col">
-                <p><?= $user->getNickname() ?></p>
-                <p>Membre depuis :</p>
+                <p id="owner-name"><?= $user->getNickname() ?></p>
+                <p id="since"><?= $user->getMembershipTime() ?></p>
             </div>
             <div class="flex-col">
                 <h3 class="mini-capital">BIBLIOTHEQUE</h3>
@@ -36,35 +39,37 @@
         </div>
     </article>
 
-    <article id="owned-books">
+    <article id="owned-books" class="flex">
         <table>
             <thead>
                 <tr>
-                    <th>PHOTO</th>
-                    <th>TITRE</th>
-                    <th>AUTEUR</th>
-                    <th>DESCRIPTION</th>
-                    <th>DISPONIBILITE</th>
-                    <th>ACTION</th>
+                    <th class="mini-capital c1">PHOTO</th>
+                    <th class="mini-capital">TITRE</th>
+                    <th class="mini-capital">AUTEUR</th>
+                    <th class="mini-capital">DESCRIPTION</th>
+                    <th class="mini-capital">DISPONIBILITE</th>
+                    <th class="mini-capital">ACTION</th>
                 </tr>
             </thead>
 
             <tbody>
+                <?php $index = 0; ?>
                 <?php foreach ($books as $book): ?>
-                    <tr>
-                        <td><img src="<?= $book->getCoverPicturePath() ?>"></td>
+                    <tr class="<?= ($index % 2 === 0) ? 'paire' : 'impaire' ?>">
+                        <td class="c1"><img src="<?= $book->getCoverPicturePath() ?>"></td>
                         <td><?= $book->getTitle() ?></td>
                         <td><?= $book->getAuthor() ?></td>
-                        <td><?= $book->getDescription(100) ?></td>
+                        <td class="description"><?= $book->getDescription(100) ?></td>
                         <td>
                             <?php if ($book->getAvailability() === 1): ?>
-                                disponible
+                                <span class="stock">disponible</span>
                             <?php else: ?>
-                                non dispo.
+                                <span class="out-stock">non dispo.</span>
                             <?php endif; ?>
                         </td>
-                        <td><a href="">Editer</a> <a href="">Supprimer</a></td>
+                        <td><a href="index.php?action=updateBook&id=<?= $book->getId() ?>" class="edit-book">Editer</a> <a href="index.php?action=deleteBook&id=<?= $book->getId() ?>" class="delete-book">Supprimer</a></td>
                     </tr>
+                <?php $index++; ?>
                 <?php endforeach; ?>
             </tbody>
         </table>
